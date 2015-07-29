345779aba427319a8f7b954ac2587c8904bb923b
ekisaame <ekisaame@gmail.com>
2015-07-29 19:21:00 +0300

    29th July updates

--- grades_assignment.py
+++ grades_assignment.py
@@ -4,24 +4,31 @@ file_location="C:/Users/emmanuelk.DI/Documents/python_tutorials/Keith_scores.txt
 file_location1="C:/Users/emmanuelk.DI/Documents/python_tutorials/Sophie_scores.txt"
 file_location2="C:/Users/emmanuelk.DI/Documents/python_tutorials/Richard_scores.txt"
 #separating the student marks
-keith_scores=file_location
-Sophie_scores=file_location1
-richard_scores=file_location2
+# keith_scores=file_location
+# Sophie_scores=file_location1
+# richard_scores=file_location2
 
+files=[file_location,file_location1,file_location2]
 
-files=[keith_scores,Sophie_scores,richard_scores]
 
 #opening the files
 def open_file(files):
     file=open(files, "r")
     data=file.readlines()
-    print (data)
+    # print (data)
     return data
+#list that will contain each students data
+data = []
 
-i=0
-for i in range (0,3):
-    current_file=files[i]
-    i=i+1
+for current_file in files:
     d=open_file(current_file)
-print (d)
-
+    #add the returned data to the data list
+    data.append(d);
+    # print data
+ 
+for d in data:
+	# grade(d)
+	print d
+# print ("Keiths marks "+ str(data[0]))
+# print ("sophies marks "+ str(data[1]))
+# print ("richards marks "+ str(data[2]))
