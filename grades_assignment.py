195dce675947a0a6a79c7257a1276692213ab5b8
ekisaame <ekisaame@gmail.com>
2015-07-29 09:27:13 +0300

    29th  July Updates

--- grades_assignment.py
+++ grades_assignment.py
@@ -1,19 +1,27 @@
-#importing all data files I will use for this programme.
+# #importing all data files I will use for this programme.
+
 file_location="C:/Users/emmanuelk.DI/Documents/python_tutorials/Keith_scores.txt"
 file_location1="C:/Users/emmanuelk.DI/Documents/python_tutorials/Sophie_scores.txt"
 file_location2="C:/Users/emmanuelk.DI/Documents/python_tutorials/Richard_scores.txt"
-files=[file_location,file_location1,file_location2]
+#separating the student marks
+keith_scores=file_location
+Sophie_scores=file_location1
+richard_scores=file_location2
+
+
+files=[keith_scores,Sophie_scores,richard_scores]
 
 #opening the files
 def open_file(files):
-	file=open(files, "r")
-	data=file.readlines()
-	print (data)
-# return data
-# i=0
-# # for i in range (0,3):
-#  		current_file=files[i]
-# 		i=i+1
-#  		d=open_file(current_file)
-# print (d)
+    file=open(files, "r")
+    data=file.readlines()
+    print (data)
+    return data
+
+i=0
+for i in range (0,3):
+    current_file=files[i]
+    i=i+1
+    d=open_file(current_file)
+print (d)
 
